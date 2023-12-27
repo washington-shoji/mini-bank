59cfbd4fe74692b7a1f46bca5cf71115c6d86ef4
washington-shoji <shoji.washington@gmail.com>
2023-12-27 17:25:08 +1100

    -added account functions header file

---
+++ Functions/AccountFunction.h
@@ -0,0 +1,9 @@
+#pragma once
+
+void write_account();
+void display_detail(int);
+void modify_account(int);
+void delete_account(int);
+void display_all();
+void deposit_withdraw(int, int);
+void intro();
